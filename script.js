123e83f64493189c7b65add041491eb951e89306
sztehlo2026 <lukacsgergely25@gmail.com>
2026-01-22 20:28:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -22,3 +22,59 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 } 
+
+/* Menü aktív állapot */
+document.querySelectorAll('.menu-item').forEach(item => {
+    item.addEventListener('click', function () {
+        document.querySelectorAll('.menu-item').forEach(i => i.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
+
+/* Menü aktív állapot */
+document.querySelectorAll('.menu-item').forEach(item => {
+    item.addEventListener('click', function () {
+        document.querySelectorAll('.menu-item').forEach(i => i.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
+
+/* JÁTÉK gomb */
+document.getElementById('playBtn').addEventListener('click', () => {
+    document.getElementById('message').textContent =
+        'A tartalom a te régiódban nem elérhető';
+});
+
+/* Szív ikon */
+const heart = document.getElementById('heart');
+heart.addEventListener('click', () => {
+    heart.classList.toggle('inactive');
+});
+
+  const video = document.querySelector('.live-photo');
+
+video.addEventListener('mouseenter', () => {
+    video.play();
+});
+
+video.addEventListener('mouseleave', () => {
+    video.pause();
+    video.currentTime = 0;
+});
+
+video.addEventListener('mouseleave', () => {
+video.pause();
+
+const rewind = setInterval(() => {
+    if (video.currentTime <= 0.1) {
+    video.currentTime = 0;
+    clearInterval(rewind);
+    } else {
+    video.currentTime -= 0.05;
+    }
+}, 16);
+});
+
+video.addEventListener('touchstart', () => {
+video.play();
+});
